6e1ff84071aeff1b1ba8ef9ad5b2e60d7cbe5ef1
sonenko <growler.ua@gmail.com>
2016-12-12 18:20:20 +0200

    WIP trouble with removing listeners

--- src/main/scala/train/DragAndDrop.scala
+++ src/main/scala/train/DragAndDrop.scala
@@ -31,34 +31,46 @@ object DragAndDrop {
     val dragEls = document.querySelectorAll("#commands > div").toList.map(_.asElement)
 
     def addDragListeners(dragEl: Element): Unit = {
-      val onDragEnd: js.Function1[DragEvent, Unit] = event =>
-        dropZoneEls.foreach(dropEl => {
+      def dropElems() =
+        if (dragEl.classList.contains("ifstation")) dropZoneEls.filter(_.classList.contains("command-placeholder-1"))
+        else dropZoneEls
+
+      var removeDropListeners = Seq[(Unit) => Unit]()
+
+      val onDragEnd: js.Function1[DragEvent, Unit] = event => {
+        console.log("onDragEnd")
+        console.log(dropElems().length)
+        removeDropListeners.foreach(x => x())
+        dropElems().foreach(dropEl => {
+          console.log(dropEl)
           dropEl.classList.remove("highlight")
         })
+      }
       val onDragStart: js.Function1[DragEvent, Unit] = event => {
-        dropZoneEls.foreach(dropEl => {
+        removeDropListeners = dropElems().map(dropEl => {
           dropEl.classList.add("highlight")
+          addDropListeners(dropEl)
         })
+        removeDropListeners.foreach(x => x())
         event.dataTransfer.setData("index", dragEl.getAttribute("data-index"))
       }
       dragEl.addEventListener("dragstart", onDragStart)
       dragEl.addEventListener("dragend", onDragEnd)
     }
 
-    def addDropListeners(dropZoneEl: Element): Unit = {
-      def addListeners(el: Element): Unit = {
-        el.addEventListener("drop", onDrop)
-        el.addEventListener("dragenter", onDragEnter)
-        el.addEventListener("dragleave", onDragLeave)
-        el.addEventListener("dragover", onDragOver)
+    def addDropListeners(dropZoneEl: Element): Unit => Unit = {
+      def addListeners(): Unit = {
+        dropZoneEl.addEventListener("drop", onDrop)
+        dropZoneEl.addEventListener("dragenter", onDragEnter)
+        dropZoneEl.addEventListener("dragleave", onDragLeave)
+        dropZoneEl.addEventListener("dragover", onDragOver)
       }
-      def removeListeners(el: Element):Unit = {
-        console.log(123123)
-
-        el.removeEventListener("drop", onDrop)
-        el.removeEventListener("dragenter", onDragEnter)
-        el.removeEventListener("dragleave", onDragLeave)
-        el.removeEventListener("dragover", onDragOver)
+      lazy val removeListeners: Unit => Unit = _ => {
+        console.log("removeListeners")
+        dropZoneEl.removeEventListener("drop", onDrop)
+        dropZoneEl.removeEventListener("dragenter", onDragEnter)
+        dropZoneEl.removeEventListener("dragleave", onDragLeave)
+        dropZoneEl.removeEventListener("dragover", onDragOver)
       }
       lazy val onDrop: js.Function1[DragEvent, Unit] = event => {
         event.preventDefault()
@@ -66,54 +78,42 @@ object DragAndDrop {
         val originalCommand = dragEls(index)
         val command = originalCommand.cloneNode(true).asElement
         command.setAttribute("draggable", "false")
+        dropZoneEl.children.toList.foreach(ch => ch.parentNode.removeChild(ch))
+        dropZoneEl.appendChild(command)
         if (dropZoneEl.classList.contains("command-placeholder-1")) {
+          val nextDropZone = dropZoneEl.parentNode.asElement.querySelector(".command-placeholder-2")
+          nextDropZone.classList.remove("highlight")
+          nextDropZone.children.toList.foreach(ch => ch.parentNode.removeChild(ch))
           // first column
           if (index != 3) {
             // any
-            console.log("first column: index != 3")
-            dropZoneEl.children.toList.foreach(ch => ch.parentNode.removeChild(ch))
-            val nextDropZone = dropZoneEl.parentNode.asElement.querySelector(".command-placeholder-2")
             dropZoneEls = dropZoneEls.filter(x => x != nextDropZone)
-            nextDropZone.classList.remove("highlight")
-            nextDropZone.children.toList.foreach(ch => ch.parentNode.removeChild(ch))
-            removeListeners(nextDropZone)
-            addListeners(nextDropZone)
-            dropZoneEl.appendChild(command)
-            dropZoneEl.classList.remove("can-drop")
           } else {
             // ifStation
-            console.log("first column: ifStation")
-            dropZoneEl.children.toList.foreach(ch => ch.parentNode.removeChild(ch))
-            val nextDropZone = dropZoneEl.parentNode.asElement.querySelector(".command-placeholder-2")
             dropZoneEls = dropZoneEls.filter(x => x != nextDropZone.children(0))
-            nextDropZone.children.toList.foreach(ch => ch.parentNode.removeChild(ch))
-            removeListeners(nextDropZone)
-            dropZoneEl.appendChild(command)
-            dropZoneEl.classList.remove("can-drop")
-            // add next drop zone
+            addDropListeners(nextDropZone)
             dropZoneEls = nextDropZone :: dropZoneEls
-            addListeners(nextDropZone)
           }
         } else if (dropZoneEl.classList.contains("command-placeholder-2")) {
           // second column
           if (index != 3) {
             // any
-            console.log(123)
-            dropZoneEl.children.toList.foreach(ch => ch.parentNode.removeChild(ch))
             dropZoneEl.appendChild(command)
-            dropZoneEl.classList.remove("can-drop")
           } else {
             // ifStation
+            console.warn("unexpected ifstation command in second column")
           }
         } else {
           console.warn("unexpected placeholder class in dropZone", dropZoneEl)
         }
+        dropZoneEl.classList.remove("can-drop")
       }
       lazy val onDragEnter: js.Function1[DragEvent, Unit] = _ => dropZoneEl.classList.add("can-drop")
       lazy val onDragLeave: js.Function1[DragEvent, Unit] = _ => dropZoneEl.classList.remove("can-drop")
       lazy val onDragOver: js.Function1[DragEvent, Unit] = event => event.preventDefault()
 
-      addListeners(dropZoneEl)
+      addListeners()
+      removeListeners
     }
 
 
